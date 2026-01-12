497e60dc29cd4f1249aea8947e649428c648e5a7
nordenq <nordenquete@proton.me>
2026-01-12 15:41:14 +0000

    google

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,9 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare global {
+  interface ImportMetaEnv {
+    readonly PUBLIC_GA_MEASUREMENT_ID?: string;
+    readonly PUBLIC_GTM_ID?: string;
+  }
+}
